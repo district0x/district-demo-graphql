ceb9f8120b10955355001e14acef269906d09448
madvas <matus.lestan@gmail.com>
2018-04-03 22:30:49 -0700

    first commit

--- src/district_demo_graphql/ui/core.cljs
+++ src/district_demo_graphql/ui/core.cljs
@@ -17,19 +17,3 @@
                    :url "http://localhost:6450/graphql"
                    :query-middlewares [(create-resolver-middleware :my-resolver {:root-value middleware-root-value})]}})
     (mount/start)))
-
-
-(comment
-  (ns some.great.app
-    (:require
-      [cool-dev.ui.now]
-      [other-dev.ui.other-module]
-      [yet-another-dev.ui.yet-another-module]))
-
-  (defn ^:export init []
-    (-> (mount/with-args
-          {:now {:some-param 1}
-           :other-module {:other-param "abc"}
-           :yet-another-dev {:yet-another-param "xyz"}})
-      (mount/start))
-    (r/render [home-page] (.getElementById js/document "app"))))
